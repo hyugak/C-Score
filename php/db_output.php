e7582ba241c1e4937965a6a8ec18da193bccc62c
あかりさん <39323965+akarisan@users.noreply.github.com>
2018-07-02 17:29:21 +0900

    Add files via upload

--- php/db_output.php
+++ php/db_output.php
@@ -2,20 +2,25 @@
 //dummy_get
 session_start();
 $_SESSION = $_GET;
-$dummy = $_SESSION[noparam];
+$dummy = $_SESSION['noparam'];
 
 //DBコネクト
 require_once('db_connect.php');
 
 //DBから点数昇順でデータ取り出す
-
+$query = "SELECT * FROM rank ORDER BY score ASC";
+$row = $dbh->query($query);
 
 //取り出したデータを変数にHTMLタグとともに格納
-
+foreach($row as $rows){
+    $score = $rows['score'];    #合計点を取り出し
+    $team = $rows['team'];      #チーム名を取り出し
+    
+    $print_html = "<tr><td><h5>{$team}</h5></td><td><h5>{$score}pt</h5></td></tr>{$print_html}";
+}
 
 
 //HTML表示
-
-
+print "{$print_html}";
 
 ?>
