a233bfe1822674d2974e7a7e3fdd3e6302468e25
あかりさん <39323965+akarisan@users.noreply.github.com>
2018-07-08 21:05:39 +0900

    Add files via upload

--- js/ajax.js
+++ js/ajax.js
@@ -36,3 +36,41 @@ function execute(container,accessurl,paramet,method) {
 			}
 		}); 
 };
+
+function executeA(container,accessurl,paramet,method) {
+	myajax = new Ajax.PeriodicalUpdater(
+		container,
+		accessurl,
+		{
+			"method": method,
+			"parameters": paramet,
+			frequency: 1, // 1時間ごとに実行
+			onSuccess: function(request) {
+				// 成功時の処理を記述
+				// alert('成功しました');
+				// jsonの値を処理する場合↓↓
+				//  var json;
+				//  eval("json="+request.responseText);
+				
+				// ↓IEでもキャッシュを読み込まずに毎回リモート接続を実行するためのコード(パラメータの書き換え)
+				var str = myajax.options.parameters;
+				var hash = str.parseQuery();
+				hash["ajax_request_id"] = Math.random();
+				hash = $H(hash);
+				myajax.options.parameters = hash.toQueryString();
+			},
+			onComplete: function(request) {
+				// 完了時の処理を記述
+				// alert('読み込みが完了しました');
+				// jsonの値を処理する場合↓↓
+				//  var json;
+				//  eval("json="+request.responseText);
+			},
+			onFailure: function(request) {
+				alert('読み込みに失敗しました');
+			},
+			onException: function (request) {
+				alert('読み込み中にエラーが発生しました');
+			}
+		}); 
+};
