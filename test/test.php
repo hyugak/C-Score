6f0e52252a29e16f634fc0ff85825d5ee255f6f8
あかりさん <39323965+akarisan@users.noreply.github.com>
2018-07-13 17:05:51 +0900

    Add files via upload

--- test/test.php
+++ test/test.php
@@ -1,26 +1,75 @@
 <?php
 var_dump($_FILES);
 
-//TEMPのURL取得
-$url = $_FILES['file']['tmp_name'];
+//アップロード処理
+$tempfile = $_FILES['file']['tmp_name'];
+$filename = $_FILES['file']['name'];
 
-var_dump($url);
+if (is_uploaded_file($tempfile)) {
+    if ( move_uploaded_file($tempfile , $filename )) {
+	echo "OK";
+    } else {
+        echo "FAILED";
+    }
+} else {
+    echo "NOENTRY";
+} 
 
 //文字列読み込み
-$json = file_get_contents($url);
-
-var_dump($json);
+$fp = fopen('c-score.json','r');
+while(!feof($fp)){
+    $line = fgets($fp);
+    $json = "{$json}{$line}";
+}
+fclose($fp);
+//var_dump($json);
 
 //文字列コンバート
 $json = mb_convert_encoding($json,'UTF-8','UTF-16LE');
 
 var_dump($json);
 
+//文字列エスケープ処理
+$json = addslashes($json);
+var_dump($json);
+
+
+echo "<br><br>";
+
+//エンコード方法の調査
+echo mb_detect_encoding($json);
+
 //連想配列化
-$array = json_decode($json,true);
+$array = json_decode($json, true);
+
+
+//エラーチェック
+    switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            echo ' - No errors';
+        break;
+        case JSON_ERROR_DEPTH:
+            echo ' - Maximum stack depth exceeded';
+        break;
+        case JSON_ERROR_STATE_MISMATCH:
+            echo ' - Underflow or the modes mismatch';
+        break;
+        case JSON_ERROR_CTRL_CHAR:
+            echo ' - Unexpected control character found';
+        break;
+        case JSON_ERROR_SYNTAX:
+            echo ' - Syntax error, malformed JSON';
+        break;
+        case JSON_ERROR_UTF8:
+            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+        break;
+        default:
+            echo ' - Unknown error';
+        break;
+    }
+
+
+
 
 var_dump($array);
-
-$file = 'test.txt';
-file_put_contents($file,$array);
 ?>
