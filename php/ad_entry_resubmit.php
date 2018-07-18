7683b8e0a64aca866af6745857e378ca31716f7f
hyuga <hyuga@hongo.wide.ad.jp>
2018-07-18 23:50:02 +0900

    write ad_all_list.php and ad_entry_resubmit.php

--- php/ad_entry_resubmit.php
+++ php/ad_entry_resubmit.php
@@ -8,13 +8,16 @@ $_SESSION = $_POST;
 
 //データ取り出し
 $id = $_SESSION['id'];
-$id = $_SESSION['team'];
-$id = $_SESSION['entry'];
+$team = $_SESSION['team'];
+$entry = $_SESSION['entry'];
 
+echo "{$id}, {$team}, {$entry}";
 //DB[rank]からIDで検索、$teamと$entryを
 //DB[rank]の「team」「entry」にUPDATEで挿入
 //エラー発生時は「../ad/index.php」にリダイレクト(try-catchを使う？)
-
+$query = "UPDATE rank SET entry={$entry}, team='{$team}' WHERE id={$id}";
+$sth = $dbh->prepare($query);
+$sth->execute();
 
 //セッション終了
 session_destroy();
